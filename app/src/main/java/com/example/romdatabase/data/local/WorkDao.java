78c1c2f5b0eda724bfc33f48ed86ee44724f9737
khoa <hatgiong@gmail.com>
2022-02-21 06:38:57 +0700

    rename

--- app/src/main/java/com/example/romdatabase/data/local/WorkDao.java
+++ app/src/main/java/com/example/romdatabase/data/local/WorkDao.java
@@ -1,4 +1,10 @@
 package com.example.romdatabase.data.local;
 
+import androidx.room.Dao;
+import androidx.room.Query;
+
+@Dao
 public interface WorkDao {
+
+    @Query("SELECT * FROM work")
 }
--- app/src/main/java/com/example/romdatabase/data/local/WorkDatabase.java
+++ app/src/main/java/com/example/romdatabase/data/local/WorkDatabase.java
@@ -1,4 +1,24 @@
 package com.example.romdatabase.data.local;
 
-public class WorkDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {WorkEntity.class}, version = 1)
+public abstract  class WorkDatabase extends RoomDatabase {
+    private static WorkDatabase instance= null;
+    public WorkDao workDao;
+    public synchronized static WorkDatabase getInstance(Context context){
+        if ( instance == null){
+            instance= Room.databaseBuilder(
+                context,
+                WorkDatabase.class,
+                "Work"
+            )
+                    .build();
+        }
+        return instance;
+    }
 }
